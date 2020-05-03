429ed105abad47fa6f31560f10720fd47291af8d
CARICOM12 <0190a@ukr.net>
2020-05-03 14:49:00 +0300

    Add files via upload

---
+++ gifToBmp/gifToBmp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace gifToBmp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i<args.Length; i++)
+            {
+
+            }
+        }
+    }
+}
